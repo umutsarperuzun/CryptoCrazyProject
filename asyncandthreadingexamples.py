6f93b6c153778732b5c2d50e11b8d2044c2efc4c
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-04-19 22:05:08 +0100

    Implement threading vs sync HTTP request performance test

--- asyncandthreadingexamples.py
+++ asyncandthreadingexamples.py
@@ -1,3 +1,51 @@
 import threading
 import requests
 import time
+
+
+def get_data_sync(urls):
+    st=time.time()
+    json_array=[]
+    for url in urls:
+        json_array.append(requests.get(url).json())
+    et=time.time()
+    elapsed_time=et-st
+    print("Execution time:",elapsed_time,"seconds.")
+    return json_array
+
+
+class ThreadingDownloader (threading.Thread):
+    json_array=[]
+    def __init__(self,url):
+        super().__init__()
+        self.url = url
+    
+    def run(self):
+        response = requests.get(self.url)
+        self.json_array.append(response.json())
+        print(self.json_array)
+        return self.json_array
+    
+def get_data_threading(urls):
+    st=time.time()
+    threads =[]
+    for url in urls:
+        t=ThreadingDownloader(url)
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+        # print(t)
+
+    et=time.time()
+    elapsed_time=et-st
+    print("Execution time:",elapsed_time,"seconds.")
+
+        
+        
+        
+urls=["http://postman-echo.com/delay/3"] * 10
+
+# get_data_threading(urls) #3.48 sec
+
+# get_data_sync(urls) #33.37 sec
